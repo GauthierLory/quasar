97b1f3aa00ffd535921ff5636bcbd2c6691dfb95
Glory <gauthier.lory@protonmail.com>
2023-01-16 16:47:22 +0100

    init project with quasar

---
+++ quasar_twitter/src/pages/LoginPage.vue
@@ -0,0 +1,18 @@
+<template>
+  <q-page class="flex flex-center">
+
+    <q-form style="max-width: 450px" class="q-gutter-sm">
+      <q-input outlined v-model="Email" label="Email to receive magic link" />
+      <q-btn label="Magic Link" type="submit" color="primary" />
+      <q-btn outline label="Login with Twitter" type="submit" color="primary" />
+    </q-form>
+  </q-page>
+</template>
+
+<script>
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  name: 'IndexPage'
+})
+</script>
